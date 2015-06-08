caec1ae1d11ebb32ab9d31b26d176c9e62bc67ae
Bobby <meatybobby@gmail.com>
2015-06-08 18:18:23 +0800

    for heroku

---
+++ app/models/message.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var bcrypt   = require('bcrypt-nodejs');
+
+// define the schema for our user model
+var messageSchema = mongoose.Schema({
+
+	sendid : String,
+	receiveid : String,
+	message : String,
+
+});
+
+// create the model for users and expose it to our app
+module.exports = mongoose.model('Message', messageSchema);
