53fdbbde1bfb8878a3bb02bede774b9be58f4410
Douglas Wu <dgrey1116@gmail.com>
2015-06-10 11:12:46 +0800

    修改friend list 的 style

--- public/js/control.js
+++ public/js/control.js
@@ -1,4 +1,33 @@
 $(function () {
+	$('#show-friend').click(function(){
+		$(this).addClass('open').hide();
+		$('#friend-list').addClass('cbp-spmenu-open');
+	});
+	$('#close-friend-list').click(function(){
+		$('#friend-list').removeClass('cbp-spmenu-open');
+		$('#show-friend').removeClass('open').show();
+	});
+	/*$('*').not('#friend-list').not('#show-friend').click(function(){
+		console.log('other area clicked');
+		$('#friend-list').removeClass('cbp-spmenu-open');
+		$('#show-friend').removeClass('open').show();
+	});*/
+	/*$('#show-friend').on('click', function(e){
+		console.log('show friend clicked');
+		$.ajax({
+				url: '/friends',
+				type: 'GET',
+				success: function(result) {
+					// Do something with the result
+					//alert('成功刪除!');
+					console.log(result);
+				}, 
+				error: function(){
+					alert('error');
+				}
+		});
+	});*/
+	
 	$('#quitBtn').click(function(){
 		//e.preventDefault();
 		if(confirm('Are you sure?')) {
@@ -15,6 +44,7 @@ $(function () {
 	
 	$('.delete-btn').on('click', function(e){
 		e.stopPropagation();
+		e.preventDefault();
 		var id = $(this).prop('id').slice(2);
 		console.log("delete click!");
 		if(confirm('確定刪除 '+id+' ?')) {
@@ -25,7 +55,7 @@ $(function () {
 				success: function(result) {
 					// Do something with the result
 					//alert('成功刪除!');
-					$('li#li-'+id).hide(100, function(){
+					$('#li-'+id).hide(100, function(){
 						$(this).remove();
 					});
 				}			
